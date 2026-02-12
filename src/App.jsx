aabfcc5f22576a833ad25eb877eda89be4aef6cb
inomjon <inomjon@gmail.com>
2026-02-12 15:42:39 +0500

    savatyasash

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+       <h1 className='text-red-600'>hello world</h1>
+                              
+    </div>
+  )
+}
